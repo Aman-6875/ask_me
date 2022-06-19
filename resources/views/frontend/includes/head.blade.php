c923ae0f6585567c3949d425484c40296d59da9e
Amanullah Aman <47345468+Aman-6875@users.noreply.github.com>
2022-06-19 18:09:10 +0600

    ....first commit....

---
+++ resources/views/frontend/includes/head.blade.php
@@ -0,0 +1,24 @@
+<head>
+
+	<!-- Basic Page Needs -->
+	<meta charset="utf-8">
+	<title>Ask me – Responsive Questions and Answers Template</title>
+	<meta name="description" content="Ask me Responsive Questions and Answers Template">
+	<meta name="author" content="2code.info">
+	
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Main Style -->
+	<link rel="stylesheet" href="/assets/style.css">
+	
+	<!-- Skins -->
+	<link rel="stylesheet" href="/assets/css/skins/skins.css">
+	
+	<!-- Responsive Style -->
+	<link rel="stylesheet" href="/assets/css/responsive.css">
+	
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="/assets/images/favicon.png">
+  
+</head>
